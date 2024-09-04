a15283136f78ee6a22c885f58aa2d6af17a4119c
Feq149 <Konrad.ciborowski@student.uj.edu.pl>
2024-09-04 23:01:45 +0200

    listview dziala holy shit

--- app/src/main/java/com/example/szachy_mobilne_2/DatabaseActivity.kt
+++ app/src/main/java/com/example/szachy_mobilne_2/DatabaseActivity.kt
@@ -1,14 +1,51 @@
 package com.example.szachy_mobilne_2
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.szachy_mobilne_2.View.DatabaseView
 
 class DatabaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.database_scroll_view)
-        val view = findViewById<DatabaseView>(R.id.database_view)
-        view.invalidate()
+        //setContentView(R.layout.database_scroll_view)
+        //val view = findViewById<DatabaseView>(R.id.database_view)
+        //view.invalidate()
+        setContentView(R.layout.database_listview)
+        val databaseListView : ListView = findViewById(R.id.database_listview_id)
+        val testStrings = arrayListOf("aaaa","bbbbb","weeeeeeeewoooooo")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("22222111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("33333111")
+        testStrings.add("111")
+        testStrings.add("111")
+        testStrings.add("111")
+        val adapter = ArrayAdapter(applicationContext,android.R.layout.simple_list_item_1,testStrings)
+        databaseListView.adapter = adapter
     }
 }
